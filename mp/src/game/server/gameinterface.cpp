a823063c8e2849d6e9ad72c6732d80372d8ec2ef
Krispy <nikodash2020@hotmail.com>
2022-11-27 20:02:19 -0500

    feat(Server): Don't load MOTD files if we are compiling a SP mod

--- mp/src/game/server/gameinterface.cpp
+++ mp/src/game/server/gameinterface.cpp
@@ -1074,8 +1074,10 @@ bool CServerGameDLL::LevelInit( const char *pMapName, char const *pMapEntities, 
 	//  to be parsed (the above code has loaded all point_template entities)
 	PrecachePointTemplates();
 
+#ifndef MAPBASE_MP
 	// load MOTD from file into stringtable
 	LoadMessageOfTheDay();
+#endif // !MAPBASE_MP
 
 	// Sometimes an ent will Remove() itself during its precache, so RemoveImmediate won't happen.
 	// This makes sure those ents get cleaned up.
