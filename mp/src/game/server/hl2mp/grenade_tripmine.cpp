57c2964163c69e27451415ecc8e2462ab616bb32
Krispy <nikodash2020@hotmail.com>
2022-11-30 11:53:10 -0500

    fix(HL2MP): fix satchel/tripmine damage and damage radius cvars
    From hrgve/hl2dm-bugfix@d2aa89f

--- mp/src/game/server/hl2mp/grenade_tripmine.cpp
+++ mp/src/game/server/hl2mp/grenade_tripmine.cpp
@@ -80,8 +80,6 @@ void CTripmineGrenade::Spawn( void )
 
 	SetCycle( 0.0f );
 	m_nBody			= 3;
-	m_flDamage		= sk_plr_dmg_tripmine.GetFloat();
-	m_DmgRadius		= sk_tripmine_radius.GetFloat();
 
 	ResetSequenceInfo( );
 	m_flPlaybackRate	= 0;
@@ -98,7 +96,8 @@ void CTripmineGrenade::Spawn( void )
 	m_iHealth = 1;
 
 	EmitSound( "TripmineGrenade.Place" );
-	SetDamage ( 200 );
+	SetDamage( sk_plr_dmg_tripmine.GetFloat() );
+	SetDamageRadius( sk_tripmine_radius.GetFloat() );
 
 	// Tripmine sits at 90 on wall so rotate back to get m_vecDir
 	QAngle angles = GetAbsAngles();
@@ -302,7 +301,7 @@ void CTripmineGrenade::DelayDeathThink( void )
 	UTIL_TraceLine ( GetAbsOrigin() + m_vecDir * 8, GetAbsOrigin() - m_vecDir * 64,  MASK_SOLID, this, COLLISION_GROUP_NONE, & tr);
 	UTIL_ScreenShake( GetAbsOrigin(), 25.0, 150.0, 1.0, 750, SHAKE_START );
 
-	ExplosionCreate( GetAbsOrigin() + m_vecDir * 8, GetAbsAngles(), m_hOwner, GetDamage(), 200, 
+	ExplosionCreate( GetAbsOrigin() + m_vecDir * 8, GetAbsAngles(), m_hOwner, GetDamage(), GetDamageRadius(),
 		SF_ENVEXPLOSION_NOSPARKS | SF_ENVEXPLOSION_NODLIGHTS | SF_ENVEXPLOSION_NOSMOKE, 0.0f, this);
 
 	UTIL_Remove( this );
