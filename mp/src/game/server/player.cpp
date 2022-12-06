f6a226d060ba222cff38904ee1f3131e5438039b
Krispy <nikodash2020@hotmail.com>
2022-12-06 11:52:50 -0500

    fix(HL2): Fix player drown sound

--- mp/src/game/server/player.cpp
+++ mp/src/game/server/player.cpp
@@ -2140,7 +2140,10 @@ void CBasePlayer::WaterMove()
 		
 		if (m_AirFinished < gpGlobals->curtime)
 		{
-			EmitSound( "Player.DrownStart" );
+			if (GetWaterLevel() == WL_Waist)
+			{
+				EmitSound( "Player.DrownStart" );
+			}
 		}
 
 		m_AirFinished = gpGlobals->curtime + AIRTIME;
